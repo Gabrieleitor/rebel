399445d25c84977c3b8de6fdee4d942eb558e253
Gabrieleitor <47004263+Gabrieleitor@users.noreply.github.com>
2020-11-07 11:31:38 -0500

    código limpio

--- src/main/java/com/starWars/rebels/service/ConcatenateMenssageService.java
+++ src/main/java/com/starWars/rebels/service/ConcatenateMenssageService.java
@@ -10,60 +10,60 @@ public class ConcatenateMenssageService {
 
     public String getMessage(List<String[]> messages) {
         Assert.notEmpty(messages, "The list of messages cannot be null");
-        String[] mensajeA = messages.get(0);
+        String[] primerMensaje = messages.get(0);
         for (int i = 1; i < messages.size(); i++) {
-            mensajeA = unifyMessage(mensajeA, messages.get(i));
+            primerMensaje = unifyMessage(primerMensaje, messages.get(i));
         }
-        return String.join(" ", mensajeA);
+        return String.join(" ", primerMensaje);
     }
 
-    private String[] unifyMessage(String[] mensajeA, String[] mensajeB) {
-        int tamanoA = Math.max(mensajeA.length, mensajeB.length);
-        int tamanoB = Math.min(mensajeA.length, mensajeB.length);
-        String[] mensajeResultado = new String[tamanoA];
-        if (tamanoA == tamanoB) {
-            unificarMensajeMismoTamano(mensajeA, mensajeB, tamanoB, mensajeResultado);
+    private String[] unifyMessage(String[] primerMensaje, String[] segundoMensaje) {
+        int tamanoMayor = Math.max(primerMensaje.length, segundoMensaje.length);
+        int tamanoMenor = Math.min(primerMensaje.length, segundoMensaje.length);
+        String[] mensajeResultado = new String[tamanoMayor];
+        if (tamanoMayor == tamanoMenor) {
+            unificarMensajeMismoTamano(primerMensaje, segundoMensaje, tamanoMenor, mensajeResultado);
         } else {
-            unificarMensajeDiferenteTamano(mensajeA.length > mensajeB.length ? mensajeA : mensajeB, mensajeA.length > mensajeB.length ? mensajeB : mensajeA, tamanoA, tamanoB, mensajeResultado);
+            unificarMensajeDiferenteTamano(primerMensaje.length > segundoMensaje.length ? primerMensaje : segundoMensaje, primerMensaje.length > segundoMensaje.length ? segundoMensaje : primerMensaje, tamanoMayor, tamanoMenor, mensajeResultado);
         }
         return mensajeResultado;
     }
 
-    private void unificarMensajeDiferenteTamano(String[] mensajeA, String[] mensajeB, int tamanoA, int tamanoB, String[] mensajeResultado) {
-        int diferenciaTamano = Math.max(tamanoA, tamanoB) - Math.min(tamanoA, tamanoB);
-        if (validarOrderAsendente(mensajeA, mensajeB, tamanoA, diferenciaTamano)) {
-            unificarMensajeAsendente(mensajeA, mensajeB, tamanoA, mensajeResultado, diferenciaTamano);
+    private void unificarMensajeDiferenteTamano(String[] mensajeMayor, String[] mensajeMenor, int tamanoMensajeMayor, int tamanoMenor, String[] mensajeResultado) {
+        int diferenciaTamano = Math.max(tamanoMensajeMayor, tamanoMenor) - Math.min(tamanoMensajeMayor, tamanoMenor);
+        if (validarOrderAsendente(mensajeMayor, mensajeMenor, tamanoMensajeMayor, diferenciaTamano)) {
+            unificarMensajeAsendente(mensajeMayor, mensajeMenor, tamanoMensajeMayor, mensajeResultado, diferenciaTamano);
         } else {
-            unificarMensajedeDecendiente(mensajeA, mensajeB, tamanoA, tamanoB, mensajeResultado);
+            unificarMensajedeDecendiente(mensajeMayor, mensajeMenor, tamanoMensajeMayor, tamanoMenor, mensajeResultado);
         }
     }
 
-    private void unificarMensajedeDecendiente(String[] mensajeA, String[] mensajeB, int tamanoA, int tamanoB, String[] mensajeResultado) {
-        for (int j = 0; j < tamanoA; j++) {
-            if (j <= tamanoB) {
-                mensajeResultado[j] = (mensajeA[j].isEmpty() && mensajeB[j].isEmpty()) ? mensajeA[j] : (getString(mensajeA, mensajeB, j, j));
+    private void unificarMensajedeDecendiente(String[] mensajeMayor, String[] mensajeMenor, int tamanoMayor, int tamanoMenor, String[] mensajeResultado) {
+        for (int j = 0; j < tamanoMayor; j++) {
+            if (j < tamanoMenor) {
+                mensajeResultado[j] = (mensajeMayor[j].isEmpty() && mensajeMenor[j].isEmpty()) ? mensajeMayor[j] : (getCompararString(mensajeMayor[j], mensajeMenor[j]));
             } else {
-                mensajeResultado[j] = mensajeA[j];
+                mensajeResultado[j] = mensajeMayor[j];
             }
 
         }
     }
 
-    private void unificarMensajeAsendente(String[] mensajeA, String[] mensajeB, int tamanoA, String[] mensajeResultado, int diferenciaTamano) {
-        for (int j = 0; j < tamanoA; j++) {
+    private void unificarMensajeAsendente(String[] mensajeMayor, String[] mensajeMenor, int tamanoMensajeMayor, String[] mensajeResultado, int diferenciaTamano) {
+        for (int j = 0; j < tamanoMensajeMayor; j++) {
             if (j >= diferenciaTamano) {
-                mensajeResultado[j] = (mensajeA[j].isEmpty() && mensajeB[j - diferenciaTamano].isEmpty()) ? mensajeA[j] : (getString(mensajeA, mensajeB, j, j - diferenciaTamano));
+                mensajeResultado[j] = (mensajeMayor[j].isEmpty() && mensajeMenor[j - diferenciaTamano].isEmpty()) ? mensajeMayor[j] : (getCompararString(mensajeMayor[j], mensajeMenor[j - diferenciaTamano]));
             } else {
-                mensajeResultado[j] = mensajeA[j];
+                mensajeResultado[j] = mensajeMayor[j];
             }
 
         }
     }
 
-    private static boolean validarOrderAsendente(String[] nombre_a, String[] nombre_b, int i, int i2) {
+    private static boolean validarOrderAsendente(String[] mensajeMayor, String[] mensajeMenor, int tamanoArrayMayor, int diferenciaDeArray) {
         boolean k = true;
-        for (int j = i2; j < i; j++) {
-            if ((!nombre_a[j].isEmpty() && !nombre_b[j - i2].isEmpty()) && (!nombre_a[j].equals(nombre_b[j - i2]))) {
+        for (int j = diferenciaDeArray; j < tamanoArrayMayor; j++) {
+            if ((!mensajeMayor[j].isEmpty() && !mensajeMenor[j - diferenciaDeArray].isEmpty()) && (!mensajeMayor[j].equals(mensajeMenor[j - diferenciaDeArray]))) {
                 k = false;
                 break;
             }
@@ -72,15 +72,15 @@ public class ConcatenateMenssageService {
         return k;
     }
 
-    private static void unificarMensajeMismoTamano(String[] nombre_a, String[] nombre_b, int i1, String[] nombre_c) {
-        for (int j = 0; j < i1; j++) {
-            nombre_c[j] = nombre_a[j].isEmpty() && nombre_b[j].isEmpty() ? nombre_a[j] : getString(nombre_a, nombre_b, j, j);
+    private static void unificarMensajeMismoTamano(String[] primerMensaje, String[] segundoMensaje, int tamanoArray, String[] mensajeResultado) {
+        for (int j = 0; j < tamanoArray; j++) {
+            mensajeResultado[j] = primerMensaje[j].isEmpty() && segundoMensaje[j].isEmpty() ? primerMensaje[j] : getCompararString(primerMensaje[j], segundoMensaje[j]);
 
         }
     }
 
-    private static String getString(String[] nombre_a, String[] nombre_b, int j, int j2) {
-        return !nombre_a[j].isEmpty() ? nombre_a[j] : nombre_b[j2];
+    private static String getCompararString(String primerMensaje, String segundoMensaje) {
+        return !primerMensaje.isEmpty() ? primerMensaje : segundoMensaje;
     }
 
 }
